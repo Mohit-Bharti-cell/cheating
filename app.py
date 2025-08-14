bb3513389ea6a9f476ddd719e1c6df2971e29387
Mohit-Bharti-cell <mohit@netfotech.in>
2025-08-14 15:29:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -82,4 +82,10 @@ def submit_test():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=False)
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=5000,
+        debug=False,
+        allow_unsafe_werkzeug=True  # Added for Render deployment
+    )
